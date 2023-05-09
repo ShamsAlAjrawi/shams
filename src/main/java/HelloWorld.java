b77d588c98b79f6a18627d09d1699266f7a204ec
Shams Al Ajrawi <salajrawi@wiley.com>
2023-05-09 10:50:58 -0700

    First program in Java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author salajrawi
+ */
+public class HelloWorld {
+     public static void main(String[] args) {
+        System.out.println("Dear Ladies!");
+         System.out.println("The material for day 2 are posted!");
+          System.out.println("Please work on them and try to submmit your GitHub classroom assignments!");
+           System.out.println("Thank you for your attending the class on time and your hard!");
+            
+          
+    }
+}
---
+++ src/main/java/newpackage/Shams.java
@@ -0,0 +1,25 @@
+package newpackage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author salajrawi
+ */
+public class Shams {
+     public static void main(String[] args) {
+        System.out.println("  *  \n * * \n*   *\n * * \n  *  \n");
+       
+        
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+        System.out.println("*   *");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+       
+    }
+}
